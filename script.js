4f9fa641b2d69f3c2f6889599647b4860277d5d1
hazeemmohamed <mhmmdhazeem9@gmail.com>
2025-10-26 21:04:09 +0300

    updated the card design

--- script.js
+++ script.js
@@ -8,3 +8,23 @@ humbur.addEventListener("click", function(){
 close.addEventListener("click", function(){
     document.querySelector(".side-navlinks").classList.remove("active");
 });
+
+
+var wantedImgContainer = document.querySelectorAll(".wanted-img-card");
+// var favouriteHeart = wantedImgContainer.querySelectorAll("svg");
+
+for (let count = 0; count < wantedImgContainer.length; count++) {
+    let heart = wantedImgContainer[count].querySelector("svg");
+    let clicked = false; 
+
+    heart.addEventListener("click", function() {
+        if (clicked == false) {
+            heart.style.fill = "orange";
+            heart.style.stroke = "orange";
+        } else {
+            heart.style.fill = "transparent";
+            heart.style.stroke = "black";
+        }
+        clicked = !clicked;
+    });
+}
